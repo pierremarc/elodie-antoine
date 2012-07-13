77e1da774bb5b2717e0be1d0f275ab164950b1e0
Pierre Marchand <pierre@oep-h.com>
2012-07-13 17:30:54 +0200

    collections 2

--- Controller/CollectionsController.php
+++ Controller/CollectionsController.php
@@ -1,18 +1,17 @@
 <?php
 
-class ImagesController extends AppController 
+class CollectionsController extends AppController 
 {
 	public $helpers = array('Html', 'Form');
 
 	public function index() 
 	{
-		$this->set('images', $this->Image->find('all'));
+		
 	}
 	
 		
 	public function view($id = null) 
 	{
-		$this->Image->id = $id;
-		$this->set('image', $this->Image->read());
+		
 	}
 }
